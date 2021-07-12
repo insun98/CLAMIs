73dcee6f47aa7f4bc423e7da7bae7d884144b6eb
insun98 <21900349@handong.edu>
2021-07-12 10:55:58 +0900

    [Fix]: Edited

--- src/main/java/net/lifove/clami/CLABI.java
+++ src/main/java/net/lifove/clami/CLABI.java
@@ -9,7 +9,7 @@ import weka.classifiers.evaluation.Evaluation;
 import weka.core.Instances;
 
 public class CLABI {
-	private static Instances instancesByCLA = null;
+	
 	private static List<Double> probabilityOfCLAMIIdx = new ArrayList<Double>();
 	private static List<Double> probabilityOfCLABIIdx = new ArrayList<Double>();
 	private static List<Double> CLAMIIdx = new ArrayList<Double>();
@@ -42,14 +42,29 @@ public class CLABI {
 	public static void getCLABIResult(Instances testInstances, Instances instances, String positiveLabel,
 			double percentileCutoff, boolean suppress, boolean experimental, String mlAlg, boolean isDegree, int sort,
 			boolean forCLABI, String fileName) {
-
-		instancesByCLA = Utils.getInstancesByCLA(instances, percentileCutoff, positiveLabel, isDegree);
+		probabilityOfCLAMIIdx.removeAll(probabilityOfCLAMIIdx) ;
+		probabilityOfCLABIIdx.removeAll(probabilityOfCLABIIdx) ;
+		CLAMIIdx.removeAll(CLAMIIdx) ;
+		CLABIIdx.removeAll(CLABIIdx) ;
+		Instances instancesByCLA = Utils.getInstancesByCLA(instances, percentileCutoff, positiveLabel, isDegree);
+		Instances final_newTestInstances = instancesByCLA;
 
 		// call getCLAMIResult with Descending order (sort = 1)
 		CLAMI.getCLAMIResult(testInstances, instances, positiveLabel, percentileCutoff, suppress, experimental, mlAlg,
 				isDegree, 1, forCLABI, fileName);
 		CLABIIdx.addAll(CLAMI.predictedLabelIdx);
 		probabilityOfCLABIIdx.addAll(CLAMI.probabilityOfIdx);
+		
+//		System.out.println("=========CLABIIdx==========");
+//		for (int i=0;i<CLABIIdx.size();i++) {
+//			System.out.println(CLABIIdx.get(i));
+//		}
+//		
+//		System.out.println("=========probabilityOfCLABIIdx==========");
+//		for (int i=0;i<probabilityOfCLABIIdx.size();i++) {
+//			System.out.println(probabilityOfCLABIIdx.get(i));
+//		}
+//		
 
 		// if Descending result is null, just execute CLAMI and return
 		if (CLABIIdx == null || probabilityOfCLABIIdx == null) {
@@ -65,65 +80,83 @@ public class CLABI {
 					mlAlg, isDegree, 0, forCLABI, fileName);
 			CLAMIIdx.addAll(CLAMI.predictedLabelIdx);
 			probabilityOfCLAMIIdx.addAll(CLAMI.probabilityOfIdx);
+			
+//			System.out.println("=========CLAMIIdx==========");
+//			for (int i=0;i<CLAMIIdx.size();i++) {
+//				System.out.println(CLAMIIdx.get(i));
+//			}
+//			
+//			System.out.println("=========probabilityOfCLAMIIdx==========");
+//			for (int i=0;i<probabilityOfCLAMIIdx.size();i++) {
+//				System.out.println(probabilityOfCLAMIIdx.get(i));
+//			}
+			
+			
 
-			Instances labeling = getLabeling(instancesByCLA, CLAMIIdx, CLABIIdx, probabilityOfCLAMIIdx,
+			Instances labeling = getLabeling(final_newTestInstances, CLAMIIdx, CLABIIdx, probabilityOfCLAMIIdx,
 					probabilityOfCLABIIdx, positiveLabel);
+			
+//			System.out.println("==========labeling==========");
+//			for (int i=0;i<labeling.size();i++) {
+//				System.out.println(labeling.get(i).classValue());
+//			}
+			
+			
 
 			int TP = 0, FP = 0, TN = 0, FN = 0;
 
 			// double[] final_prediction;
 			String mlAlgorithm = mlAlg != null && !mlAlg.equals("") ? mlAlg : "weka.classifiers.functions.Logistic";
 			// check if there are no instances in any one of two classes.
-			if (labeling.attributeStats(labeling.classIndex()).nominalCounts[0] != 0
-					&& labeling.attributeStats(labeling.classIndex()).nominalCounts[1] != 0) {
-
+			if(labeling.attributeStats(labeling.classIndex()).nominalCounts[0]!=0 &&
+					labeling.attributeStats(labeling.classIndex()).nominalCounts[1]!=0){
+			
 				try {
-					Classifier final_classifier = (Classifier) weka.core.Utils.forName(Classifier.class, mlAlgorithm,
-							null);
+					Classifier final_classifier = (Classifier) weka.core.Utils.forName(Classifier.class, mlAlgorithm, null);
 					final_classifier.buildClassifier(labeling);
-
+					
 					// Print CLAMI results
-					for (int instIdx = 0; instIdx < instancesByCLA.numInstances(); instIdx++) {
-						double final_predictedLabelIdx = final_classifier.classifyInstance(instancesByCLA.get(instIdx));
-
-						if (!suppress) {
-							System.out.println("CLABI: Instance " + (instIdx + 1) + " predicted as, "
-									+ instancesByCLA.classAttribute().value((int) final_predictedLabelIdx) +
-									// ((newTestInstances.classAttribute().indexOfValue(positiveLabel))==predictedLabelIdx?"buggy":"clean")
-									// +
-									", (Actual class: " + Utils.getStringValueOfInstanceLabel(instancesByCLA, instIdx)
-									+ ") ");
+					
+					for(int instIdx = 0; instIdx < final_newTestInstances.numInstances(); instIdx++){
+						double final_predictedLabelIdx = final_classifier.classifyInstance(final_newTestInstances.get(instIdx));
+					System.out.println("final predicted Label Index : " + final_predictedLabelIdx);
+						
+						
+						if(!suppress) {
+							System.out.println("CLAMI: Instance " + (instIdx+1) + " predicted as, " + 
+									final_newTestInstances.classAttribute().value((int)final_predictedLabelIdx)	+
+									//((newTestInstances.classAttribute().indexOfValue(positiveLabel))==predictedLabelIdx?"buggy":"clean") +
+									", (Actual class: " + Utils.getStringValueOfInstanceLabel(final_newTestInstances,instIdx) + ") ");
 						}
-
-						// final_prediction =
-						// final_classifier.distributionForInstance(instancesByCLA.get(instIdx));
-
+						
+						
+					
 						// compute T/F/P/N for the original instances labeled.
-						if (!Double.isNaN(instances.get(instIdx).classValue())) {
-
-							if (final_predictedLabelIdx == instances.get(instIdx).classValue()) {
-								if (final_predictedLabelIdx == instances.attribute(instances.classIndex())
-										.indexOfValue(positiveLabel)) {
+						if(!Double.isNaN(instances.get(instIdx).classValue())){
+							
+							if(final_predictedLabelIdx==instances.get(instIdx).classValue()){
+								if(final_predictedLabelIdx==instances.attribute(instances.classIndex()).indexOfValue(positiveLabel)) {
 									TP++;
-								} else {
+								}
+								else {
 									TN++;
 								}
-							} else {
-								if (final_predictedLabelIdx == instances.attribute(instances.classIndex())
-										.indexOfValue(positiveLabel)) {
+							}else{
+								if(final_predictedLabelIdx==instances.attribute(instances.classIndex()).indexOfValue(positiveLabel)) {
 									FP++;
-								} else {
+								}
+								else {
 									FN++;
 								}
 							}
 						}
 					}
-
+					
 					Evaluation eval = new Evaluation(labeling);
-					eval.evaluateModel(final_classifier, instancesByCLA);
+					eval.evaluateModel(final_classifier, final_newTestInstances);
 
 					if (TP + TN + FP + FN > 0) {
-						Utils.printEvaluationResult(TP, TN, FP, FN, eval, instancesByCLA, positiveLabel, experimental,
+						Utils.printEvaluationResult(TP, TN, FP, FN, eval, final_newTestInstances, positiveLabel, experimental,
 								fileName);
 					}
 
